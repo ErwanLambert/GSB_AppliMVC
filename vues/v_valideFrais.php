bc9eceeb58ddcde38937216a03934dfcff385381
ErwanLambert <erwanlambert@hotmail.fr>
2021-12-14 14:35:21 +0100

    vue ValideFrais fonctionne

--- vues/v_valideFrais.php
+++ vues/v_valideFrais.php
@@ -23,7 +23,7 @@
                 }
                 foreach ($infoFraisForfait as $frais) {
                     $idLibelle = $frais['idfrais'];
-                    $fraiskm = $frais['quantite'];
+                    $fraiskm = $frais['fraiskm'];
                     if ($idLibelle !== 'KM') {
                        $montant = $frais['quantite'] * $frais['prix'];
                     } else {
@@ -65,29 +65,31 @@
                 <th>Prix</th>
             </tr>
             <?php
-                $idLibelle = $infoFraisForfait['idfrais'];
-                $libelleFrais = $infoFraisForfait['libelle'];
-                $quantite = $infoFraisForfait['quantite'];
-                $prix = $infoFraisForfait['prix'];
-                $fraiskm = $infoFraisForfait['fraiskm']; ?>
+                foreach ($infoFraisForfait as $fraisFF) {
+                    $idLibelleFF = $fraisFF['idfrais'];
+                    $libelleFraisFF = $fraisFF['libelle'];
+                    $quantiteFF = $fraisFF['quantite'];
+                    $prixFF = $fraisFF['prix'];
+                    $fraiskmFF = $fraisFF['fraiskm'];?>
                     <tr>
-                        <td><?php echo $libelleFrais ?></td>
-                        <td><?php echo $idLibelle ?></td>
+                        <td><?php echo $libelleFraisFF ?></td>
+                        <td><?php echo $idLibelleFF ?></td>
                         <td>
                             <div class="form-group">
                                 <input type="text" id="idFrais" 
-                                        name="lesFrais[<?php echo $idLibelle ?>]"
+                                        name="lesFrais[<?php echo $idLibelleFF ?>]"
                                         size="1" maxlength="5" 
-                                        value="<?php echo $quantite ?>" 
+                                        value="<?php echo $quantiteFF ?>" 
                                         class="form-control">
-                                <?php if ($idLibelle !== 'KM') { ?>
-                                    <td><?php echo $prix ?></td>
+                                <?php if ($idLibelleFF !== 'KM') { ?>
+                                    <td><?php echo $prixFF ?></td>
                                 <?php } else { ?>
-                                    <td><?php echo $fraiskm ?></td>
+                                    <td><?php echo $fraiskmFF ?></td>
                                 <?php } ?>
                             </div>
                         </td>
                     </tr>
+                <?php } ?>
         </table>
     </div>
     <input id="okElemForf" type="submit" value="Corriger" class="btn btn-success" 
@@ -96,7 +98,7 @@
            role="button">
 </form>
 </br></br>
-            <?php } else { ?>
+                <?php } else { ?>
 <div class="panel panel-info" style="border-color: #FF7302;">
     <div class="panel-heading" style="border-color: #FF7302; background-color: #FF7302; color: white;">Pas d'éléments forfaitisés</div>
 </div></br></br>
@@ -115,46 +117,47 @@ if($infoFraisHorsForfait){ ?>
                 <th></th>
             </tr>
             <?php
-            $date = $infoFraisHorsForfait['date'];
-            $datee = implode('-', array_reverse(explode('/', $date))); /* transforme une date fr en une date us -> 29/10/2020 en 2020-10-29 */
-            $libellehorsFrais = $infoFraisHorsForfait['libelle'];
-            $montantHF = $infoFraisHorsForfait['montant'];
-            $id = $infoFraisHorsForfait['id'];?>
+            foreach ($infoFraisHorsForfait as $fraisHF) {
+            $dateHF = $fraisHF['date'];
+            $dateeHF = implode('-', array_reverse(explode('/', $dateHF))); /* transforme une date fr en une date us -> 29/10/2020 en 2020-10-29 */
+            $libellehorsFraisHF = $fraisHF['libelle'];
+            $montantHF = $fraisHF['montant'];
+            $idHF = $fraisHF['id'];?>
             <tr>
                 <td><div class="form-group">
                         <label for="date"></label>
                         <input type="date" 
-                               name="lesDates[<?php echo $id ?>]"
+                               name="lesDates[<?php echo $idHF ?>]"
                                size="10" maxlength="15" 
-                               value="<?php echo $datee ?>">
+                               value="<?php echo $dateeHF ?>">
                     </div></td>
                     <td><div class="form-group">
                         <label for="libelle"></label>
                         <input type="text" 
-                               name="lesLibelles[<?php echo $id ?>]"
+                               name="lesLibelles[<?php echo $idHF ?>]"
                                size="15" maxlength="40" 
-                               value="<?php echo $libellehorsFrais ?>">
+                               value="<?php echo $libellehorsFraisHF ?>">
                     </div></td>
                     <td><div class="form-group">
                         <label for="montant"></label>
                         <input type="text" 
-                               name="lesMontants[<?php echo $id ?>]"
+                               name="lesMontants[<?php echo $idHF ?>]"
                                size="10" maxlength="15" 
                                value="<?php echo $montantHF ?>"> €
                     </div></td>
-                    <td><input id="okElemHorsForf" name="corriger[<?php echo $id ?>]" type="submit" value="Corriger" class="btn btn-success" 
+                    <td><input id="okElemHorsForf" name="corriger[<?php echo $idHF ?>]" type="submit" value="Corriger" class="btn btn-success" 
                                accept=""role="button"> 
                         <input id="annuler" type="reset" value="Réinitialiser" class="btn btn-warning"" 
                                accept=""role="button">
-                        <a href="index.php?uc=ValiderFicheDeFrais&action=supprimerFrais&idFrais=<?php echo $id ?>&mois=<?php echo $infoFraisHorsForfait['mois'] ?>&idVisiteur=<?php echo $_SESSION['visiteur'] ?> " 
+                        <a href="index.php?uc=ValiderFicheDeFrais&action=supprimerFrais&idFrais=<?php echo $idHF ?>&mois=<?php echo $mois ?>&idVisiteur=<?php echo $_SESSION['visiteur'] ?>" 
                            type="reset" class="btn btn-danger" role="button"
-                           onclick="<?php return confirm('Voulez-vous vraiment supprimer ou reporter ce frais hors forfait?');?>">Supprimer</a>
+                           onclick="return confirm('Voulez-vous vraiment supprimer ou reporter ce frais hors forfait?');">Supprimer</a>
                     </td>
                 </tr>
         </table>
     </div>
 </form>
-<?php }else{ ?>
+            <?php }}else{ ?>
             <div class="panel panel-info" style="border-color: #FF7302;">
                     <div class="panel-heading" style="border-color: #FF7302; background-color: #FF7302; color: white;">Pas d'éléments hors forfais</div>
             </div>
